5559b6d1edafc89beb3c66d9b016fe6731405d4b
Dante Expo <dante0im@gmail.com>
2022-05-19 21:03:53 -0300

    Added meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
 	return (
 		<Html lang="en" translate="no">
-			<Head />
+			<Head>
+				<meta property="og:title" content="Dante Expósito" />
+				<meta property="og:type" content="Portfolio" />
+				<meta property="og:url" content="https://www.danteexpo.com/" />
+				<meta property="og:image" content="https://i.imgur.com/30oilmT.png" />
+			</Head>
 			<body>
 				<Main />
 				<NextScript />
